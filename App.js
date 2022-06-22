53a0e3cb1a9e9e0cf758334618501a77d7cba67c
luisinh077 <lhl.lima07@gmail.com>
2022-06-22 15:00:50 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Form from './src/components/Form';
+import Title from './src/components/title';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Title/>
+      <Form/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e0e5e5',
+    paddingTop: 80,
+  },
+});
